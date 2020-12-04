65ec60f408af5e6c7c6c4e43ea5a2ea21370a609
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-04 19:07:01 +0330

    Update q.py

--- q.py
+++ q.py
@@ -18,7 +18,8 @@ print(Fore.RED + """
 
 print("Pleass Enter Your 1 or 2 ")
 print(Fore.RED + "     [1] "+ Fore.GREEN + "Ip WebSite" + Fore.BLUE + " ;) ")
-print(Fore.RED + "     [2] "+ Fore.GREEN + "Your Ip" + Fore.BLUE + " ;) \n\n")
+print(Fore.RED + "     [2] "+ Fore.GREEN + "Your Ip" + Fore.BLUE + " ;) ")
+print(Fore.RED + "     [3] "+ Fore.GREEN + "Cloud Flare For WebSite" + Fore.BLUE + " ;) ")
 n= int(input(Fore.GREEN + "Enter Your Number 1 / 2 " + Fore.BLUE + "==>  " ))
         
 
@@ -63,3 +64,38 @@ if n == 2:
     except:
       pass
   __2__()
+
+# _____________________________________________________________________________________________________
+#======================================================================================================
+
+if n == 3:
+
+
+    import os
+    import sys
+    import time
+    import socket
+    from colorama import Fore
+
+    def __3__():
+        try:
+            site = input(Fore.RED + "Enter Your Address Web Site " + Fore.YELLOW + "==>  ")
+            my_list = ["www",'ftp', 'cpanel', 'webmail', 'localhost', 'local', 'mysql', 'forum', 'driect-connect', 'blog', 'vb', 'forums', 'home', 'direct', 'forums', 'mail', 'access', 'admin', 'administrator', 'email', 'downloads', 'ssh', 'owa', 'bbs', 'webmin', 'paralel', 'parallels', 'www0', 'www', 'www1', 'www2', 'www3', 'www4', 'www5', 'shop', 'api', 'blogs', 'test', 'mx1', 'cdn', 'mysql', 'mail1', 'secure', 'server', 'ns1', 'ns2', 'smtp', 'vpn', 'm', 'mail2', 'postal', 'support', 'web', 'dev']
+            for item in my_list:
+                host = item + "." + site
+                bypass = socket.gethostbyname(str(host))
+                print(Fore.GREEN + "Your Ip "+ Fore.YELLOW + "==> " + Fore.RED + str(bypass) + Fore.YELLOW + " | " + Fore.RED + str(host))
+        except:
+            pass
+    __3__()
+
+
+
+
+
+
+
+
+
+
+  
