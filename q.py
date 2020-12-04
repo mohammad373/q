011a43653b5bc85b4a8104d55238443486c71118
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-04 18:01:17 +0330

    Update and rename README.md to q.py

---
+++ q.py
@@ -0,0 +1,32 @@
+# q
+
+from colorama import Fore
+print(Fore.RED + """)
+        ╔═════╗     ╔═════╗          
+        ║ ███ ║     ║ ███ ║                      
+        ║ ███ ║     ║ ███ ║       
+        ║ ███ ╚═════╝ ███ ║
+        ║ ███████████████ ║         
+        ║ ███████████████ ║     
+        ║ ███ ╔═════╗ ███ ║      
+        ║ ███ ║     ║ ███ ║      
+        ║ ███ ║     ║ ███ ║      
+        ╚═════╝     ╚═════╝ 
+                        """)
+
+import os
+import socket
+import sys
+import os
+def __1__():
+    try:
+        print(Fore.RED +"[!]" + Fore.GREEN + "Hello . Welcome Back " +  Fore.RED";)")
+        site = input(Fore.RED + "Pleass Enter You Target Address " + Fore.YELLOW + "==>  ")
+        if not "http" in site or not "https" in site:
+            site = ("http://" + site)
+        soc = socket.gethostbyname(str(site))
+        print(Fore.GREEN + "Your Ip " + Fore.YELLOW + "==>  "  + Fore.RED + str(soc))
+        print(Fore.GREEN + "End " + Fore.YELLOW + ";)")
+    except:
+        pass
+__1__()
