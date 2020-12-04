e18b80fd412b38d614ec2bb76746596bfb546c80
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-04 18:08:45 +0330

    Update q.py

--- q.py
+++ q.py
@@ -14,16 +14,12 @@ print(Fore.RED + """
                 ╚═════╝     ╚═════╝ 
                                                 """)
 
-import os
+
 import socket
-import sys
-import os
 def __1__():
     try:
         site = input(Fore.RED + "Pleass Enter You Target Address " + Fore.YELLOW + "==>  ")
-        if not "http" in site or not "https" in site:
-            site = ("http://" + site)
-        soc = socket.gethostbyname(str(site))
+        soc = socket.gethostbyname(site)
         print(Fore.GREEN + "Your Ip " + Fore.YELLOW + "==>  "  + Fore.RED + str(soc))
         print(Fore.GREEN + "End " + Fore.YELLOW + ";)")
     except:
