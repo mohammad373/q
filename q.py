fa2fcf47ee7297182c94a1174af72d7fbbf3f75f
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-04 23:12:10 +0330

    Update q.py

--- q.py
+++ q.py
@@ -217,15 +217,17 @@ def __6__():
     try:
         target = input(Fore.GREEN +"Enter Your Address WebSite" + Fore.YELLOW + " ==>  ")
         if not "https://" in target or not "http://" in target:
-            target = ("http://" + target + "/")
+            pass
         else:
-            target = (target + "/")
+            target = ( "http://+ target)
         for item in my_list:
-            req = requests.get(target + item)
+            time.sleep(0.1)
+            ur = (target + "/" + item)          
+            req = requests.get(ur)
             if rqe.status_code == 200 or req.tutle_code == 405:
-                print(Fore.GREEN+"[+]"+Fore.GREEN + target + Fore.GREEN + "Found")
+                print(Fore.GREEN+"[+]"+Fore.GREEN + ur + Fore.GREEN + "Found")
             else:
-                print(Fore.RED +"[+]"+Fore.RED +target + Fore.YELLOW + "Not Found")
+                print(Fore.RED +"[+]"+Fore.RED +ur + Fore.YELLOW + "Not Found")
     except:
         pass
 __6__()
