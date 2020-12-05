69a23c61c9484bd2ce4b9d41b6accc7b9a8df890
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-05 16:23:57 +0330

    Update q.py

--- q.py
+++ q.py
@@ -192,9 +192,9 @@ if n == 6:
                       rq = url+ "/" + i
                       r = requests.get(rq)
                       if r.status_code   == 200 or r.status_code   == 405:
-                          print("Yes")
+                          print(Fore.GREEN + rq  + Fore.GREEN + " Found")
                       else:
-                          print("no")
+                          print(Fore.RED + rq  + Fore.RED + " Not Found")
           __6__()
 
 
