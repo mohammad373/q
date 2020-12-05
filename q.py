f0ab19ce6c595455f6ce601e0bdd7cf9208d635e
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-05 17:19:08 +0330

    Update q.py

--- q.py
+++ q.py
@@ -253,7 +253,7 @@ if n == 7 :
                         sys.exit()
                     except:
                         pass
-                info = ipapi.loction((ip=i, key=None, field=None)
+                info = ipapi.location(ip=i, key=None, field=None)
                 try:
                     print(Fore.GREEN + "Your Ip " + Fore.YELLOW + "==> " + Fore.RED + info["ip"])
                     print(Fore.GREEN + "City "    + Fore.YELLOW + "==>  " + Fore.RED + info["city"])
